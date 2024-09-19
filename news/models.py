24a98dd379fa5a46b5b6101a832d6a9d45e7abd1
Maxim Turenko <turenkomaksim099@gmail.com>
2024-09-19 13:15:41 +0300

    add models Redactor, Topic & Newspaper

--- news/models.py
+++ news/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="topic_newspapers"
+    )
+    publisher = models.ManyToManyField(Redactor, related_name="redactor_newspapers")
